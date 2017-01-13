8373a04d92d67a148eb60dd119ef15875cda5316
John Szwaronek <johnsz9999@gmail.com>
2017-01-13 14:44:58 -0500

    New standards

--- examples/make-diffs-step.sh
+++ examples/make-diffs-step.sh
@@ -34,7 +34,7 @@ buildDiff 01/rest/2.js 01/websocket/1.js _diff/01-websocket-1
 buildDiff 01/common/public/rest.html 01/common/public/socketio.html _diff/01-websocket-socketio
 buildDiff 01/websocket/1.js 01/hooks/1.js _diff/01-hooks-1
 buildDiff 01/hooks/1.js 01/hooks/2.js _diff/01-hooks-2
-buildDiff 01/common/public/feathers-app.js 02/app/public/feathers-app.js _diff/02-app-feathers-app
+buildDiff 01/common/public/feathers-app-del.js 02/app/public/feathers-app.js _diff/02-app-feathers-app
 buildDiff 02/app/src/app.js 02/service/src/app.js _diff/02-service-src-app
 buildDiffDir 02/app/ 02/app1/ _diff/02-app1
 buildDiff 02/app1/src/services/teams/hooks/index.js 02/service/src/services/teams/hooks/index.js _diff/02-service-teams-hooks
